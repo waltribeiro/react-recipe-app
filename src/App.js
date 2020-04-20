b7f67038e1763fb426db4e3b73a5a45acc4392f2
WaltRibeiro <walt@fororchestra.com>
2020-04-20 19:50:44 -0400

    created form and API keys

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const APP_ID = "05c2d0cb";
+  const APP_KEY = "c2c2a2fe0efcf347b4bea463809269c0";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+    return (
+      <div className="App">
+        <form className="search-form">
+          <input className="search-bar" type="text" />
+          <button className="search-button" type="submit">Search</button>
+        </form>
+      </div>
   );
 }
 
